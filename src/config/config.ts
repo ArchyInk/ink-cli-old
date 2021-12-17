2542f8016edebceda21fbd8a52a80ad20a6bd1f2
archy <archybool@gmail.com>
2021-12-17 17:26:07 +0800

    添加find-up依赖，寻找根目录

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,13 +3,17 @@
  * @author: Archy
  * @Date: 2021-12-17 16:07:44
  * @LastEditors: Archy
- * @LastEditTime: 2021-12-17 16:46:59
+ * @LastEditTime: 2021-12-17 17:24:50
  * @FilePath: \ink-cli\src\config\config.ts
  * @description: 
  */
-import { CWD } from "../shared/constant"
+import findUp from "find-up"
+import { CWD,CONFIG_DEFAULT_PATH } from "../shared/constant"
 import { getRootPath } from "../shared/utils"
 
 export const getInkConfig = async () => {
   getRootPath()
+  CONFIG_DEFAULT_PATH
 }
+
+getInkConfig()
